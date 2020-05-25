ab6aa78b6a30ff0c55407a7c9af10420671bbe83
koktoh <koktoh96@gmail.com>
2020-05-25 13:48:12 +0900

    create hid_glove

---
+++ keyboards/hid_glove/keymaps/default/keymap.c
@@ -0,0 +1,90 @@
+/* Copyright 2020 koktoh
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+#include "paw3204.h"
+#include "pointing_device.h"
+
+
+// Defines names for use in layer keycodes and the keymap
+enum layer_names {
+    _BASE,
+};
+
+// Defines the keycodes used by our macros in process_record_user
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* Base */
+    [_BASE] = LAYOUT(
+        KC_BTN1, KC_A, KC_B, KC_C,
+        KC_BTN2, KC_1, KC_2, KC_3
+    )
+};
+
+void encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) { /* First encoder */
+        if (clockwise) {
+            tap_code(KC_WH_D);
+        } else {
+            tap_code(KC_WH_U);
+        }
+    }
+}
+
+void matrix_init_user(void) {
+    init_paw3204();
+}
+
+report_mouse_t mouse_rep;
+
+void keyboard_post_init_user() {
+    debug_enable = true;
+    debug_mouse = true;
+}
+
+void matrix_scan_user(void) {
+    static int  cnt;
+    static bool paw_ready;
+    if (cnt++ % 50 == 0) {
+        uint8_t pid = read_pid_paw3204();
+        if (pid == 0x30) {
+            dprint("paw3204 OK\n");
+            paw_ready = true;
+        } else {
+            dprintf("paw3204 NG:%d\n", pid);
+            paw_ready = false;
+        }
+    }
+
+    if (paw_ready) {
+        uint8_t stat;
+        int8_t x, y;
+
+        read_paw3204(&stat, &x, &y);
+        mouse_rep.buttons = 0;
+        mouse_rep.h       = 0;
+        mouse_rep.v       = 0;
+        mouse_rep.x       = y;
+        mouse_rep.y       = -x;
+
+        if (cnt % 10 == 0) {
+            dprintf("stat:%3d x:%4d y:%4d\n", stat, mouse_rep.x, mouse_rep.y);
+        }
+
+        if (stat & 0x80) {
+            pointing_device_set_report(mouse_rep);
+        }
+    }
+}
---
+++ keyboards/hid_glove/keymaps/default/readme.md
@@ -0,0 +1,1 @@
+# The default keymap for hid_glove
