3eba7e14465dbc38516aa9b338d4b87f20937aaa
koktoh <koktoh96@gmail.com>
2022-05-12 19:05:36 +0900

    modify rgb lighting effects

--- keyboards/dear_creators/config.h
+++ keyboards/dear_creators/config.h
@@ -77,9 +77,9 @@
 /*== all animations enable ==*/
 //#    define RGBLIGHT_ANIMATIONS
 /*== or choose animations ==*/
-#    define RGBLIGHT_EFFECT_BREATHING
+// #    define RGBLIGHT_EFFECT_BREATHING
 #    define RGBLIGHT_EFFECT_RAINBOW_MOOD
-//#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
 //#    define RGBLIGHT_EFFECT_SNAKE
 //#    define RGBLIGHT_EFFECT_KNIGHT
 //#    define RGBLIGHT_EFFECT_CHRISTMAS
